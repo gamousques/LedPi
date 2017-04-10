afa2cff5097ea19b0dd9a6692f6c7e3a7718d737
Gaston Mousques <mousques@hotmail.com>
2017-04-10 09:17:49 -0300

    version inicial

---
+++ led1.cpp
@@ -0,0 +1,50 @@
+/*
+ * led1.cpp
+ * 
+ * Copyright 2017  <pi@raspberrypi>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+#include <stdio.h>
+#include <iostream>
+#include <wiringPi.h>
+
+#define  LedPin    0
+
+int main(void)
+{
+	if(wiringPiSetup() == -1){ //when initialize wiring failed,print messageto screen
+		printf("setup wiringPi failed !");
+		return 1; 
+	}
+	printf("linker LedPin : GPIO %d(wiringPi pin)\n",LedPin); //when initialize wiring successfully,print message to screen
+	
+	pinMode(LedPin, OUTPUT);
+
+	while(1){
+			digitalWrite(LedPin, LOW);  //led on
+			printf("led on...\n");
+			delay(500);
+			digitalWrite(LedPin, HIGH);  //led off
+			printf("...led off\n");
+			delay(500);
+	}
+
+	return 0;
+}
